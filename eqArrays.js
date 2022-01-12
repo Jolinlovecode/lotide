9876ac6b8af88b4498c7ca8c92b53a5d9247da58
Hongling Zhang <jolinhlzhang@email.com>
2022-01-12 15:33:34 -0700

    Implement eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+let result = false
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length === secondArray.length) {
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] === secondArray[i]) {
+        result = true;
+      }
+    }
+  }
+  return result;
+}
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
