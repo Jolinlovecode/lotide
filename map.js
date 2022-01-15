438e6f7ed04ab834e16c0a70d0d31b628a05b02c
Hongling Zhang <jolinhlzhang@email.com>
2022-01-15 15:23:38 -0700

    map function:array & callback

--- map.js
+++ map.js
@@ -5,8 +5,10 @@ const map = function(array, callback) {
   }
   return results;
 };
-const results1 = map(words, word => word[0]);
 
 const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+
 
 console.log(results1);
